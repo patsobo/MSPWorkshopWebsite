ea774218d34d82deb0ad932428f40d135a3f1527
Patrick Sobolewski <psobolew@mymail.mines.edu>
2016-02-11 19:03:01 -0700

    Pokemon!

---
+++ data/pokemonData.js
@@ -0,0 +1,47 @@
+﻿/**
+    This is the data source for your application. In the real world, you would either get this data from a data base or
+    from an API 
+**/
+exports.pokemonTypeName = {
+    fire : 'Fire',
+    water : 'Water',
+    grass : 'Grass'
+}
+exports.fire = [
+    {
+    name: 'Charmander',
+    moveset: ["Scratch",
+            "Ember",
+            "Smokescreen",
+            "Fire Spin"
+        ],
+    photo : 'http://bulbapedia.bulbagarden.net/wiki/File:004Charmander.png'
+}
+]
+
+
+
+exports.water = [
+    {
+        name: 'Squirtle',
+        moveset: ["Bubble",
+            "Bite",
+            "Water Gun",
+            "Withdraw"
+        ],
+        photo : 'http://bulbapedia.bulbagarden.net/wiki/File:007Squirtle.png'
+    }
+]
+
+
+exports.grass = [
+    {
+        name: 'Bulbasaur',
+        moveset: ["Leech Seed",
+            "Vine Whip",
+            "Razor Leaf",
+            "Growl"
+        ],
+        photo : 'http://bulbapedia.bulbagarden.net/wiki/File:001Bulbasaur.png'
+    }
+]
